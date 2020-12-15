ed5abb0fe8bece0902317e6c0a8047e08ac45899
ivo-saurio <ivan.trabajo.ar@hotmail.com>
2020-12-15 18:54:05 -0300

    Se crearon las rutas en routes/productos.js (GET, POST, PUT Y DELETE), se escribieron las funtion en productosController.js, faltan funcionalidades

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,13 +1,48 @@
 const fs = require('fs')
 const path = require('path')
 
+let productos = fs.readFileSync(path.join(__dirname, '../database/productos.json'), 'utf8');
+let arrayProductos = JSON.parse(productos)
+
+
 module.exports = {
     todo: function(req, res) {
-
-        let productos = fs.readFileSync(path.join(__dirname, '../database/productos.json'), 'utf8');
-        //let arrayProductos = JSON.parse(productos)
-        //return res.render('productdetail', {productosEnLaVista: arrayProductos})
         return res.send(productos)
+    },
+
+    create: function(req, res){
+        return res.render ('productadd', {productosEnLaVista: productos})
+    },
+
+    porId: function(req, res){
+        for(let i = 0; i < arrayProductos.length; i++){
+            if(req.params.idProducto == arrayProductos[i].id){
+                if(req.params.categoria){
+                    return res.send(arrayProductos[i].category)
+                } else{
+                    return res.send(arrayProductos[i])
+                }
+            }
+        }
+        return res.send("Este producto no existe")
+    },
+
+    sendcreation: function(req, res){
+
+    },
+
+    edit: function(req, res){
+
+    },
+
+    sendedit: function(req, res){
+
+    },
+
+    delete: function(req, res){
+
+    },
+        
     }
-}
+
 
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -4,6 +4,42 @@ const router = express.Router()
 
 const productosController = require('../controllers/productosController');
 
+// /products (GET)
+// Listado de productos
 router.get('/', productosController.todo);
+
+// 2. /products/create (GET)
+// Formulario de creación de productos
+router.get('/create', productosController.create);
+
+// 3. /products/ :id (GET)
+// Detalle de un producto particular
+router.get('/detalle/:idProducto/:categoria?', productosController.porId);
+
+// 4. /products (POST)
+// Acción de creación (a donde se envía el formulario)
+router.post('/', productosController.sendcreation);
+
+// 5. /products/ :id /edit (GET)
+// Formulario de edición de productos
+router.get('/:id/edit', productosController.edit);
+
+// 6. /products/ :id (PUT)
+// Acción de edición (a donde se envía el formulario):
+router.put('/:id', productosController.sendedit);
+
+// 7. /products/ :id (DELETE)
+// Acción de borrado
+router.delete('/:id', productosController.delete);
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
