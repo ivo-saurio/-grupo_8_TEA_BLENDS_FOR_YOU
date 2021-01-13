cce0543a7dbc7f8983d1f714f71063d66f4a6dd2
ivo <ivan.trabajo.ar@hotmail.com>
2021-01-13 11:14:35 -0300

    Instalacion de recursos cookie-parser, morgan y method-override, http-errors

--- src/app.js
+++ src/app.js
@@ -1,27 +1,50 @@
+var createError = require('http-errors');
 const express = require('express');
-const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const methodOverride = require ("method-override");
 
+const app = express();
+
+//RUTAS REQUERIDAS
 const mainRouter = require('./routes/main');
 const productosRouter = require('./routes/productos');
 const usersRouter = require('./routes/users')
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, '../public')));
-
+//VIEW ENGINE SETUP
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+//RECURSOS
 app.use('/', mainRouter);
 app.use('/users', usersRouter);
 app.use('/productos', productosRouter)
-
-
-
 
 app.listen(process.env.PORT || 3000, function(){
     console.log('Servidor corriendo en el puerto 3000');
     console.log("http://localhost:3000");
 })
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
