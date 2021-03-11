61da2d8783ff63efd18d19984f406ec65ea6bb6c
MaxiRawe <maxirawe@hotmail.com>
2021-03-11 19:48:06 -0300

    Se agrego etiqueta small en register.ejs y ademas se empezo a usar validaciones de registro de usuario

--- public/js/registerFront.js
+++ public/js/registerFront.js
@@ -1,6 +1,58 @@
-//if ( (input_name.value=="") || (input_email.value=="") || (!input_email.value.includes("@")) ) {
-//    alert("Complete los campos con el formato correspondiente");
-//}
+let qs = function(element) {
+    return document.querySelector(element);
+}
 
-//if ((inputName.value!='') && (inputName.value.match(/^[A-Za-z]+$/)) ){
-//}
+window.addEventListener('load', function(){
+
+    let form = qs('#formRegister')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+        let inputName = qs('#name');
+            inputSurname = qs('#surname');
+            inputAvatar = qs('#avatar');
+            inputEmail = qs('#email');
+            inputPassword = qs('#password');
+            inputRepassword = qs('#repassword');
+
+
+        let errorName = qs('#errorName')
+            errorSurname = qs('#errorSurname')
+            errorAvatar = qs('#errorAvatar')
+            errorEmail = qs('#errorEmail')
+            errorPassword = qs('#errorPassword')
+            errorRepassword = qs('#errorRepassword')
+
+    let errores = {}
+
+    if(inputName.value.length == 0){
+        errores.name = 'este campo es obligatorio'
+        errorName.innerText = errores.name
+    } else if(inputName.value.length < 2){
+        errores.name = 'El nombre debe tener más de 2 caracteres'
+        errorName.innerText = errores.name
+
+    } else if(inputName.value.match(/^[A-Za-z]+$/)){
+        errores.name = 'Solo letras'
+        errorName.innerText = errores.name
+
+    } else {
+        errorName.innerText = ""
+    }
+
+    console.log(errores);
+    })
+})
+
+
+
+
+
+
+
+// if ( (input_name.value=="") || (input_email.value=="") || (!input_email.value.includes("@")) ) {
+//    alert("Complete los campos con el formato correspondiente");
+// }
+
+// if ((inputName.value!='') && (inputName.value.match(/^[A-Za-z]+$/)) ){
+// }
