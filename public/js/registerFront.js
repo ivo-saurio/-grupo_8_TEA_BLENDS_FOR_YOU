c1450284caa0778911ff3f8088d1656e1c25fc47
MaxiRawe <maxirawe@hotmail.com>
2021-03-13 00:19:54 -0300

    Se modifico el header y se agregaron validaciones front en register

--- public/js/registerFront.js
+++ public/js/registerFront.js
@@ -4,9 +4,9 @@ let qs = function(element) {
 
 window.addEventListener('load', function(){
 
-    let form = qs('#formRegister')
+    let register = qs('#formRegister')
 
-form.addEventListener('submit', function(e){
+register.addEventListener('submit', function(e){
     e.preventDefault()
         let inputName = qs('#name');
             inputSurname = qs('#surname');
@@ -16,14 +16,21 @@ form.addEventListener('submit', function(e){
             inputRepassword = qs('#repassword');
 
 
-        let errorName = qs('#errorName')
-            errorSurname = qs('#errorSurname')
-            errorAvatar = qs('#errorAvatar')
-            errorEmail = qs('#errorEmail')
-            errorPassword = qs('#errorPassword')
-            errorRepassword = qs('#errorRepassword')
+        let errorName = qs('#errorName');
+            errorSurname = qs('#errorSurname');
+            errorAvatar = qs('#errorAvatar');
+            errorEmail = qs('#errorEmail');
+            errorPassword = qs('#errorPassword');
+            errorRepassword = qs('#errorRepassword');
+
+    
+    let regexOnlyLetters = /^[A-Za-z]+$/;
+    let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    let regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/
 
     let errores = {}
+
+    //VALIDACION NOMBRE
 
     if(inputName.value.length == 0){
         errores.name = 'este campo es obligatorio'
@@ -31,13 +38,49 @@ form.addEventListener('submit', function(e){
     } else if(inputName.value.length < 2){
         errores.name = 'El nombre debe tener más de 2 caracteres'
         errorName.innerText = errores.name
-
-    } else if(inputName.value.match(/^[A-Za-z]+$/)){
-        errores.name = 'Solo letras'
+    } else if(!inputName.value.match(regexOnlyLetters)){
+        errores.name = 'El nombre debe tener solo letras'
         errorName.innerText = errores.name
-
     } else {
         errorName.innerText = ""
+    }
+
+    //VALIDACION APELLIDO
+
+    if(inputSurname.value.length == 0){
+        errores.surname = 'este campo es obligatorio'
+        errorSurname.innerText = errores.surname
+    } else if(inputSurname.value.length < 2){
+        errores.surname = 'El nombre debe tener más de 2 caracteres'
+        errorSurname.innerText = errores.surname
+    } else if(!inputSurname.value.match(regexOnlyLetters)){
+        errores.surname = 'El nombre debe tener solo letras'
+        errorSurname.innerText = errores.surname
+    } else {
+        errorSurname.innerText = ""
+    }
+
+    //VALIDACION EMAIL
+
+    if(!inputEmail.value.match(regexEmail)){
+        errores.email = 'Ingresar email valido'
+        errorEmail.innerText = errores.email
+    } else {
+        errorEmail.innerText = ""
+    }
+
+    //VALIDACION CONTRASEÑA
+
+    if(!inputPassword.value.match(regexPassword)){
+        errores.password = 'Su contraseña debe tener más de 6 caracteres'
+        errorPassword.innerText = errores.password
+    } else {
+        errorPassword.innerText = ""
+    }
+
+
+    if(Object.keys(errores).length == 0) {
+        register.submit()
     }
 
     console.log(errores);
