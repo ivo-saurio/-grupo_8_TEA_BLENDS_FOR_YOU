a0d0700e774c53e9b224aa27b5f1db885c4466ed
MaxiRawe <maxirawe@hotmail.com>
2021-03-22 23:50:42 -0300

    Ordenado algunos detalles de codigo

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(methodOverride("_method"));
-app.use(session ({secret: 'sarasa'}));
+app.use(session ({secret: 'botannys'}));
 app.use(sessionMiddleware);
 app.use(rememberMiddleware);
 
@@ -47,18 +47,3 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("http://localhost:3000");
 })
 
-// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-  //next(createError(404));
-//});
-
-// error handler
-//app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  //res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  //res.status(err.status || 500);
-  //res.redirect('https://http.cat/[status_code]');
-//});
