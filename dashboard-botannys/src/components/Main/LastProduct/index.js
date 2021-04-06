aefd922204166f2daa57aabc4755d795746e61ae
MaxiRawe <maxirawe@hotmail.com>
2021-04-06 20:57:51 -0300

    Se modificó un error en Last Product

--- dashboard-botannys/src/components/Main/LastProduct/index.js
+++ dashboard-botannys/src/components/Main/LastProduct/index.js
@@ -9,7 +9,7 @@ function LastProduct(){
 				</div>
 				    <div className="card-body">
 						<div className="text-center">
-							<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" src="assets/images/product_dummy.svg" alt=""/>
+							<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: "25rem"}} src="assets/images/product_dummy.svg" alt=""/>
 						</div>
 						    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
 							<a target="_blank" rel="nofollow" href="/">Detalle del Producto </a>
